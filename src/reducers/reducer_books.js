798a7b34b5b2e68ff7cfda302bcd6dbc71c5bd88
GabrielTuculina <gtuculina@yahoo.com>
2018-07-13 14:53:10 +0300

    Add files via upload
    only the src for the boilerplate of https://github.com/StephenGrider/ReduxSimpleStarter

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The tile of a legend', pages: 101},
+        {title: 'Harry Potter and the Holly Grail', pages: 1299},
+        {title: 'Elisabeth got diabetes', pages: 56},
+        {title: 'Welcome back to Narnia', pages: 6432}
+    ]
+}
